0f96b172ca7819a782e9136ace7e4bea5aef76a8
Franco Andrés Brusatti <fbrusatti@gmail.com>
2010-05-13 20:02:56 -0300

    added in_place_select helper

--- lib/in_place_macros_helper.rb
+++ lib/in_place_macros_helper.rb
@@ -77,4 +77,65 @@ module InPlaceMacrosHelper
     tag = content_tag(tag_options.delete(:tag), h(instance_tag.value(instance_tag.object)),tag_options)
     return tag + in_place_editor(tag_options[:id], in_place_editor_options)
   end
+
+  ##
+  # Renders an in-place select similar to in_place_editor.  Options are the same as those supported by
+  # InPlaceMacrosHelper.in_place_editor(), plus some extra ones to deal with the list:
+  #
+  # <tt>:collection</tt>::              The collection that will be used to build the list options
+  # <tt>:load_collection_url</tt>::     A URL that will return the collection in JSON format
+  # <tt>:loading_collection_text</tt>:: Text to display while the collection is loading
+  # <tt>:loading_class_name</tt>::      Class applied to form while the collection is loading
+  ##
+  def in_place_select(field_id, options = {})
+    function =  "new Ajax.InPlaceCollectionEditor("
+    function << "'#{field_id}', "
+    function << "'#{url_for(options[:url])}'"
+
+    js_options = {}
+    js_options['cancelText'] = %('#{options[:cancel_text]}') if options[:cancel_text]
+    js_options['okText'] = %('#{options[:save_text]}') if options[:save_text]
+    js_options['loadingText'] = %('#{options[:loading_text]}') if options[:loading_text]
+    js_options['savingText'] = %('#{options[:saving_text]}') if options[:saving_text]
+    js_options['rows'] = options[:rows] if options[:rows]
+    js_options['cols'] = options[:cols] if options[:cols]
+    js_options['size'] = options[:size] if options[:size]
+    js_options['externalControl'] = "'#{options[:external_control]}'" if options[:external_control]
+    js_options['loadTextURL'] = "'#{url_for(options[:load_text_url])}'" if options[:load_text_url]
+    js_options['ajaxOptions'] = options[:options] if options[:options]
+    js_options['evalScripts'] = options[:script] if options[:script]
+    js_options['onComplete'] = options[:on_complete] if options[:on_complete]
+    js_options['callback']   = "function(form) { return #{options[:with]} }" if options[:with]
+    js_options['clickToEditText'] = %('#{options[:click_to_edit_text]}') if options[:click_to_edit_text]
+
+    js_options['collection'] = %(#{js_collection_for(options[:collection])}) if options[:collection]
+    js_options['loadCollectionURL'] = %('#{url_for(options[:load_collection_url])}') if options[:load_collection_url]
+    js_options['loadingCollectionText'] = %('#{options[:loading_collection_text]}') if options[:loading_collection_text]
+    js_options['loadingClassName'] = %('#{options[:loading_class_name]}') if options[:loading_class_name]
+
+    function << (', ' + options_for_javascript(js_options)) unless js_options.empty?
+    function << ')'
+    javascript_tag(function)
+  end
+
+  private
+    ##
+    # Converts the given collection to a javascript string suitable for rendering options in a select list.
+    # The collection key becomes the option value, while the collection value becomes the body of the <option> tags.
+    ##
+    def js_collection_for(collection)
+      js = '['
+      collection.each { |key, value| js << "[#{to_javascript(value)},#{to_javascript(key)}]," }
+      js = js.chop
+      js << ']'
+    end
+
+    ##
+    # Surrounds the given value with single quotes if it's not a number so JavaScript will render/process the select
+    # option values correctly.
+    ##
+    def to_javascript(value)
+      return value if value.is_a?(Numeric)
+      "'#{value}'"
+    end
 end
